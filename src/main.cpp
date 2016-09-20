d509683313978ec22e3d342a78aa09b0d79e1e1b
Tim Holt <atimholt@gmail.com>
2016-09-19 23:28:30 -0700

    Add a window

--- src/main.cpp
+++ src/main.cpp
@@ -5,26 +5,46 @@
 #define DOCTEST_CONFIG_IMPLEMENT
 #include "doctest.h"
 
-#include <vulkan/vulkan.h>
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
 
 #include <iostream>
+
+const int kWidth  = 800;
+const int kHeight = 600;
 
 class HelloTriangleApplication
 {
  public:
   void run()
   {
+    initWindow();
     initVulkan();
     mainLoop();
   }
 
  private:
+  GLFWwindow* window;
+
+  void initWindow()
+  {
+    glfwInit();
+
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+    window = glfwCreateWindow(kWidth, kHeight, "Vulkan", nullptr, nullptr);
+  }
+
   void initVulkan()
   {
   }
 
   void mainLoop()
   {
+    while (!glfwWindowShouldClose(window)) {
+      glfwPollEvents();
+    }
   }
 };
 
