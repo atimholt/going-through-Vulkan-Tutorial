dccabf38dd4feef37ed4ba6c45969270ad4915d4
Tim Holt <atimholt@gmail.com>
2019-06-17 16:48:41 -0700

    Add testing code to main

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,22 @@
 
 #include <cstdlib>
 
+#define DOCTEST_CONFIG_IMPLEMENT
+#include "doctest.h"
+
 int main(int argc, char* argv[])
 {
-  return EXIT_SUCCESS;
+  ////////////////////////////// Testing Stuff ////
+  doctest::Context context;                      //
+  context.setOption("no-breaks", true);          //
+  context.applyCommandLine(argc, argv);          //
+  auto test_results = context.run();             //
+  if (context.shouldExit()) return test_results; //
+  /////////////////////////////////////////////////
+
+  // My code here
+
+  return EXIT_SUCCESS + test_results;
 }
 
 // vim:set et ts=2 sw=2 sts=2:
