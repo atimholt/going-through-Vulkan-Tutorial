d4aa2a75a7da5af6e83c1798207deb889cb07623
Tim Holt <atimholt@gmail.com>
2019-06-17 17:18:25 -0700

    LESSON: Development environment https://vulkan-tutorial.com/Development_environment

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,18 @@
 /// \file    main.cpp
 
-#include <cstdlib>
-
 #define DOCTEST_CONFIG_IMPLEMENT
 #include "doctest.h"
+
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include <glm/mat4x4.hpp>
+#include <glm/vec4.hpp>
+
+#include <cstdlib>
+#include <ostream>
 
 int main(int argc, char* argv[])
 {
@@ -15,7 +24,28 @@ int main(int argc, char* argv[])
   if (context.shouldExit()) return test_results; //
   /////////////////////////////////////////////////
 
-  // My code here
+  glfwInit();
+
+  glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+  GLFWwindow* window =
+      glfwCreateWindow(800, 600, "Vulkan window", nullptr, nullptr);
+
+  uint32_t extension_count = 0;
+  vkEnumerateInstanceExtensionProperties(nullptr, &extension_count, nullptr);
+
+  std::cout << extension_count << " extensions supported" << std::endl;
+
+  glm::mat4 matrix;
+  glm::vec4 vec;
+  auto test = matrix * vec;
+
+  while (!glfwWindowShouldClose(window)) {
+    glfwPollEvents();
+  }
+
+  glfwDestroyWindow(window);
+
+  glfwTerminate();
 
   return EXIT_SUCCESS + test_results;
 }
