6c30d858bf478c5b3736861aa78375a875f361fd
Tim Holt <atimholt@gmail.com>
2019-06-18 23:00:46 -0700

    LESSON: Dra…—Setup—Valid…: Using validation layers
    https://vulkan-tutorial.com/Drawing_a_triangle/Setup/Validation_layers#page_Using_validation_layers

--- src/VulkanHandler.cpp
+++ src/VulkanHandler.cpp
@@ -4,7 +4,16 @@
 #include <GLFW/glfw3.h>
 
 #include <algorithm>
+#include <set>
+#include <string>
 #include <vector>
+
+#define WHOLE(container) std::begin(container), std::end(container)
+#define CWHOLE(container) std::cbegin(container), std::cend(container)
+
+const std::vector<const char*> VulkanHandler::k_validation_layers{
+    "VK_LAYER_KHRONOS_validation"};
+
 
 VulkanHandler::VulkanHandler()
 {
@@ -25,6 +34,10 @@ std::vector<const char*> getExtensions()
 
 void VulkanHandler::createInstance()
 {
+  if (k_enable_validation_layers && !checkValidationLayerSupport()) {
+    throw std::runtime_error("validation layers requested, but not available!");
+  }
+
   vk::ApplicationInfo app_info{
       "Hello Triangle",         // pApplicationName
       VK_MAKE_VERSION(1, 0, 0), // application version
@@ -37,8 +50,31 @@ void VulkanHandler::createInstance()
 
   vk::InstanceCreateInfo create_info{{}, &app_info, 0, {},
       static_cast<uint32_t>(extensions.size()), extensions.data()};
+  if (k_enable_validation_layers) {
+    create_info.enabledLayerCount =
+        static_cast<uint32_t>(k_validation_layers.size());
+    create_info.ppEnabledLayerNames = k_validation_layers.data();
+  }
 
   vk_instance_ = vk::createInstanceUnique(create_info);
+}
+
+bool VulkanHandler::checkValidationLayerSupport()
+{
+  using namespace std;
+
+  set<string> available_layer_names{};
+  for (const auto& layer_properties : vk::enumerateInstanceLayerProperties()) {
+    available_layer_names.emplace(layer_properties.layerName);
+  }
+
+  set<string> validation_layers_sorted{};
+  for (const auto& layer : VulkanHandler::k_validation_layers) {
+    validation_layers_sorted.emplace(layer);
+  }
+
+  return includes(
+      CWHOLE(available_layer_names), CWHOLE(validation_layers_sorted));
 }
 
 // vim:set et ts=2 sw=0 sts=0:
--- src/VulkanHandler.hpp
+++ src/VulkanHandler.hpp
@@ -5,11 +5,20 @@
 
 struct VulkanHandler
 {
+  static const std::vector<const char*> k_validation_layers;
+#ifdef NDEBUG
+  static const bool k_enable_validation_layers = false;
+#else
+  static const bool k_enable_validation_layers = true;
+#endif
+
   vk::UniqueInstance vk_instance_;
 
   VulkanHandler();
 
   void createInstance();
+
+  bool checkValidationLayerSupport();
 };
 
 #endif // !VULKAN_HANDLER_HPP_IS_INCLUDED
